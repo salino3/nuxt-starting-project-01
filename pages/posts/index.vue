62c9403470e4f70fe0a53fad6afb484ba5fc3ee2
salino3 <flaviobcn012@gmail.com>
2023-07-12 14:44:48 +0200

    fisrt commit

---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="posts-page">
+
+    </div>
+</template>
+
+<script >
+
+</script>
+
+<style  scope>
+.post-page {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+
+</style>
